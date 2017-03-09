ac07cbae69ceeddc6bcad36f1a707bfb3dbf93e9
ayy <filbaun@gmail.com>
2017-03-09 22:54:50 +0300

    done

---
+++ lab02/FBlab.cpp
@@ -0,0 +1,75 @@
+#include "iostream"
+#include "mlisp.h"
+
+double half__interval__method(double a, double b);
+double FBtry(double neg__point, double pos__point);
+bool close__enough_Q(double x, double y);
+double average(double x, double y);
+double root (double a, double b);
+double fun(double z);
+const double tolerance = 0.00001;
+
+double half__interval__method(double a, double b) {
+    double a__value = 0.;
+    double b__value = 0.;
+    a__value = fun(a);
+    b__value = fun(b);
+    return((a__value > 0. || b__value < 0.) ? FBtry(a,b)
+    	: (a__value < 0. && b__value > 0.) ? FBtry(b,a)
+    	: (b + 1)); 
+}
+
+double FBtry(double neg__point, double pos__point) {
+    double midpoint = 0.;
+    double test__value = 0.;
+    midpoint = average(neg__point, pos__point);
+    display("+");
+    return (close__enough_Q(neg__point, pos__point) ? midpoint
+        : true ? test__value = fun(midpoint),
+    	    (test__value > 0.0) ? FBtry(neg__point, midpoint)
+            :(test__value < 0.0) ? FBtry(midpoint, pos__point)
+	    :  midpoint
+            : _infinity);
+}
+
+bool close__enough_Q(double x, double y) {
+    return(abs(x - y) < tolerance);
+}
+
+double average(double x, double y) {
+    return((x+y)/2.0);
+}
+
+double root (double a, double b) {
+    double temp = 0.0;
+    temp = half__interval__method(a, b);
+    newline();
+    display("interval=\t[");
+    display(a);
+    display(" , ");
+    display(b);
+    display("]\n");
+    display("discrepancy=\t");
+    display(fun(temp)); newline();
+    display("root=\t\t");
+    (temp - b - 1) == 0 ? display("[bad]"): display("[good]");
+    return temp;
+}
+
+
+
+double fun(double z) {
+    z = z - 103.0/104.0 - 1.0/pi;
+    return ( 4.0 * log(z) * log(z) + 6.0 * log(z) - 5.0);
+}
+
+int main() {
+    display("FB variant 3\n");
+    root(3.0,4.0);
+    return 0;
+}
+
+
+
+  
+    
---
+++ lab03/FBlab.ss
@@ -0,0 +1,37 @@
+(define (cc amount largest-coin)
+  (cond ((or(= amount 0) (= largest-coin 1)) 1)
+        ((not(and(> amount 0) (> largest-coin 0))) 0)
+        (else(+ (cc amount (next-coin largest-coin)) (cc (- amount largest-coin) largest-coin)))
+        )
+  )
+
+(define (count-change amount)
+  (cc amount LAGEST-COIN)
+  )
+
+(define (next-coin coin)
+  (cond ((= coin 50) 25)
+        ((= coin 25) 3)
+        ((= coin 3) 2)
+        ((= coin 2) 1)
+        (else 0))
+  )
+(define LAST-DIGIT-OF-GROUP-NUMBER 4)
+(define GR-AMOUNT
+  (* LAST-DIGIT-OF-GROUP-NUMBER 10)
+  )
+
+(define LAGEST-COIN 50)
+
+
+(display" FB variant 2")
+(newline)
+(display" 1-2-3-25-50")
+(newline)
+(display" count-change for 100 \t= ")
+(display(count-change 100))
+(newline)
+(display" count-change for ")
+(display GR-AMOUNT)
+(display" \t= ")
+(display(count-change GR-AMOUNT))
